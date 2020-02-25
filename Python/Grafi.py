e5ac453be9cfd3e29362200c74c8deb211329f80
Luca Giavelli <LGiave@users.noreply.github.com>
2020-02-25 18:47:10 +0100

    _

--- Python/Grafi.py
+++ Python/Grafi.py
@@ -1,11 +1,31 @@
-Matrix=[]
-Nodi=int(input("inserire n° nodi"))
+def user2adj():
+    Matrix=[]
+    Nodi=int(input("inserire n° nodi"))
 
-for i in range(0,Nodi):
-    col=input("inserire collegamenti")
-    collegamenti=int(col.split(","))
-    colonna=[0 for i in range(0,Nodi)]
-    for n in collegamenti:
-        colonna[n]=1
+    for i in range(0,Nodi):
+        collegamenti=[int(k) for k in input(f"Inserire collegamenti di {i}: ").split(",")]
+        colonna=[0 for i in range(0,Nodi)]
+        for n in collegamenti:
+            if(n!= -1):
+                colonna[n]=1
 
-    Matrix.append(colonna)
+        Matrix.append(colonna)
+    return Matrix
+    
+
+def adj2dict(Matrix):
+    d={}
+    for l in range(0,len(Matrix)):
+        d[l]=Matrix[l]
+    return d
+
+def dict2adj(d):
+    m=[]
+    for _,v in d.items():
+        m.append(v)
+    print(m)
+    return m
+
+if __name__ == "__main__":
+    dict2adj(adj2dict(user2adj()))
+
