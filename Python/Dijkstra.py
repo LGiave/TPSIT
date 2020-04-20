4592fda3cb06e0b26eef33f156127cd4cba29d50
Luca Giavelli <48553961+LGiave@users.noreply.github.com>
2020-04-20 17:55:46 +0200

    Update Dijkstra.py

--- Python/Dijkstra.py
+++ Python/Dijkstra.py
@@ -1,48 +1,43 @@
-mat = [[0,1,2,0],[1,0,3,1],[2,3,0,2],[0,1,2,0]]
-INFINITO = 1000000000000000000000000
+import math
 
-def dijkstra (graph, source):
-    q = []
-    dist = []
-    prev = []
-    for v,n in enumerate(graph):
-        dist[v] = INFINITO
-        prev[v] = None
-        q.append(v)
+graph = [[0,1,4,0,0,0,0,0],
+         [1,0,0,0,0,2,0,0],
+         [4,0,0,5,0,0,0,0],
+         [0,0,5,0,1,3,0,0],
+         [0,0,0,1,0,0,2,3],
+         [0,2,0,3,0,0,0,0],
+         [0,0,0,0,2,0,0,4],
+         [0,0,0,0,3,0,4,0]]
+
+labelList = [math.inf] * len(graph) #inizializzazione delle label (moltiplico per il numero di nodi per le righe)
+labelList[0] = 0                    #distanza dal nodo sorgente = 0
+
+unexploredNode = [0,1,2,3,4,5,6,7]   #nodi da esplorare
+
+cntPasso=0
+
+while len(unexploredNode)>0:
+    print("Passo numero: %d"% cntPasso)
+    print("Nodi inesplorati: ", unexploredNode)
+    print("Lista delle label: ", labelList)
+    print("-----------------------------------------------")
+    #scelgo il nodo con label minore(k)
+    infiniteList = [math.inf] * len(graph)
     
-    dist[source] = 0
-
-    while q != None:
-
-        minn = INFINITO
-        for i in len(dist):
-            if (dist[i]<minn):
-                minn = i
-        
-        u = [q[minn], dist[minn]]
-
-        q.remove(minn)
-
-        mat = neighbor(graph, minn)
-
-        for v,m in enumerate(mat):
-            alt = dist[minn] + mat[v]
-            if (alt < dist[v]):
-                dist[v] = alt
-                prev[v] = minn
+    for i in unexploredNode:
+        infiniteList[i]=labelList[i]    #valore della labelList[i] 
     
-    return dist, prev
+    minimun = min(infiniteList)   #trovo il minimo
 
-def neighbor(graph, index):
-    matN = []
-    for i, c in graph:
-        if (c == index):
-            matN.append(graph[i][c])
-    return matN
-
-def main():
-    indice = int(input("inserisci un nodo del grafo da cui partire"))
-    print(dijkstra(mat, indice))
-
-if __name__ == "main":
-    main()
+    if(minimun==math.inf):  #controllo per uscire dal ciclo
+        break
+    
+    currentNode = infiniteList.index(minimun)  #prendo l'indice del valore minimo
+    for node, weight in enumerate(graph[currentNode]):       #ciclo for con ricerca dei nodi collegati a k per indici non ancora esplorati
+        if(weight!=0):   #verifico che sia collegato e che non sia se stesso
+            newLabel = labelList[currentNode] + weight
+            if(newLabel<labelList[node]):
+               labelList[node] = newLabel     #per ogni nuovo nodo sommo (valore label(k)+peso arco di quel nodo) il valore della sua label e controllo se sono minori così sostituisco sennò niente
+    
+    cntPasso=cntPasso+1
+unexploredNode.remove(currentNode) #rimuovo l'indice visitato
